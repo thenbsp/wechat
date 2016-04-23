c339f1663bd90a8556376ac6d3dd81642cf34649
thenbsp <thenbsp@gmail.com>
2016-04-23 19:14:38 +0800

    Update Util.php

--- src/Bridge/Util.php
+++ src/Bridge/Util.php
@@ -58,11 +58,18 @@ class Util
      */
     public static function filterNickname($nickname)
     {
-        $nickname = preg_replace('/[\x{1F600}-\x{1F64F}]/u', '', $nickname);
-        $nickname = preg_replace('/[\x{1F300}-\x{1F5FF}]/u', '', $nickname);
-        $nickname = preg_replace('/[\x{1F680}-\x{1F6FF}]/u', '', $nickname);
-        $nickname = preg_replace('/[\x{2600}-\x{26FF}]/u', '', $nickname);
-        $nickname = preg_replace('/[\x{2700}-\x{27BF}]/u', '', $nickname);
+        $pattern = array(
+            '/\xEE[\x80-\xBF][\x80-\xBF]/',
+            '/\xEF[\x81-\x83][\x80-\xBF]/',
+            '/[\x{1F600}-\x{1F64F}]/u',
+            '/[\x{1F300}-\x{1F5FF}]/u',
+            '/[\x{1F680}-\x{1F6FF}]/u',
+            '/[\x{2600}-\x{26FF}]/u',
+            '/[\x{2700}-\x{27BF}]/u',
+            '/[\x{20E3}]/u'
+        );
+
+        $nickname = preg_replace($pattern, '', $nickname);
 
         return trim($nickname);
     }
